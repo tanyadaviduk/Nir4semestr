96704b309a8eeac85a825d73be3d2531275d8803
tanyadaviduk <d.tanusha96@mail.ru>
2016-12-20 22:03:31 +0300

    Delete GenerateGlnL1.m

--- GenerateGlnL1.m
+++
@@ -1,9 +0,0 @@
-
-function[s]=GenerateGlnL1(t , PN , Tpn , A , f ,phi)
-length(PN)=511;
-PNSize = length(PN);
-ind=mod(fix(t/Tpn),PNsize)+1;% массив индексов
-PSP=PN(ind);             % Растянутая последовательность 
-ref = A*cos(2*pi * f * t + phi);  % Несущая
-s = PSP .* ref;                 % Формирование сигнала
-end
